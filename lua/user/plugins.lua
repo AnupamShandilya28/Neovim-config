061d2b982746e93c624d7ff936d7864d0e00b117
AnupamShandilya28 <anupam.shandilya@mindbodyonline.com>
2023-12-24 23:53:48 +0530

    added color and copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,11 +44,11 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-
+  use "github/copilot.vim"
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
-
+ -- use "lunarvim/darkplus.nvim"
+  use "folke/tokyonight.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
